0a47c8621d763a6cb05c4c940b1ca70bc358c5b8
hanzalla <hanzialam034@gmail.com>
2021-08-28 13:58:03 +0500

    sidenave

--- Admin/src/app/app.component.ts
+++ Admin/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'Admin Dashboard';
-  sideBarOpen = true;
+  sideBarOpen = false;
 
 
   ngOnInit() { }
